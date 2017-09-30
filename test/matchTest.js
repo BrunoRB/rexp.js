7453f1ef3dd6015bdacd3b45f61dc44995489ce2
BrunoRB <brunorb8@hotmail.com>
2017-09-30 16:50:30 -0300

    bootstraping

--- test/matchTest.js
+++ test/matchTest.js
@@ -1,9 +1,3 @@
 const assert = require('assert');
 const G = require('./../src/engine.js');
-
-
-describe('First-follow', function() {
-	it('should compute the sets of a simple grammar ' + grammars.simple, function() {
-
-    });
-});
+// TODO
